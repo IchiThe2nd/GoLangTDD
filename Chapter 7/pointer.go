54323769a46db61382447e027dbf5db32eab150e
David Swensson <David.swensson@yahoo.com>
2022-10-08 02:45:59 -0700

    Upto dictionary/maps

---
+++ Chapter 7/pointer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf(" balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+var ErrinsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrinsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
