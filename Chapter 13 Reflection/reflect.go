fb099ee29647f02f2ee4b3adab1bfc5866a1a06e
Jeremy Huether <jhuether@chrgescience.com>
2022-10-17 08:10:13 -0700

    reflect explain

--- Chapter 13 Reflection/reflect.go
+++ Chapter 13 Reflection/reflect.go
@@ -6,16 +6,33 @@ import "reflect"
 // func(input string) [an empty func with a string input declaration]
 // so fn := func(input string) // as passed from TestWalk and as defined here
 // how does fn(return "Chris") modify input.
+
+/*
+The test passes in a function that looks like this.  It's probably the confusing part.
+func(input string) {
+				got = append(got, input)
+			}
+*/
 func walk(x interface{}, fn func(input string)) {
+
+	// get concrete values for the interface, assign to val
 	val := reflect.ValueOf(x)
 
+	// for each field in val
 	for i := 0; i < val.NumField(); i++ {
+		
 		//field is now chris or london depending on i
+
+		// Set field to the val iterators currently indexed position
 		field := val.Field(i)
 		// how is below assining things to input? what is this doing?
 		//test case below shoul now be fn("Chris") for i=0. is input being
 		//redeclared/assigned inside the fn() call. I think im missing a key concept
 		// around the value of input
+
+		// Take the current field as a string and use it as a parameter to our anonymous function
 		fn(field.String())
+
+		// The function we passed in appends got to the given input, which should be the field we passed in when calling the function.
 	}
 }
--- Chapter 13 Reflection/reflect_test.go
+++ Chapter 13 Reflection/reflect_test.go
@@ -6,39 +6,36 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	// cases is a slice of structs with slice 0 being the test named struct with on string. Input is struct{Name field} == chris and .ExpectedCalls == []string with chris @ [0]
-	// it has a struct interface Name string .Name is chris and .ExepectedCalls is chris
+
 	cases := []struct {
 		Name          string
 		Input         interface{}
 		ExpectedCalls []string
 	}{
 		{
-			"struct A with one string field",
+			"struct with one string field",
 			struct {
 				Name string
 			}{"Chris"},
 			[]string{"Chris"},
 		},
 		{
-			"struct with 2 string field ",
-			struct {
-				Name string
-				City string
-			}{"Chris", "London"},
-			[]string{"Chris", "London"},
-		},
+    "struct with two string fields",
+    struct {
+        Name string
+        City string
+    }{"Chris", "London"},
+    []string{"Chris", "London"},
+},
 	}
 
 	for _, test := range cases {
 		t.Run(test.Name, func(t *testing.T) {
 			var got []string
 			walk(test.Input, func(input string) {
-				// func(input string) passed to walk as second argument
-				//final line of walk is fn("chris") which assigned "chris" to input
-				//
 				got = append(got, input)
 			})
+
 			if !reflect.DeepEqual(got, test.ExpectedCalls) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
